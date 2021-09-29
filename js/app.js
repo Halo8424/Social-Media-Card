982e927ccc504fbfbb997366f8890ed8b8177acd
Richard.C <r.conde@live.com>
2021-09-29 19:05:39 -0400

    appended the instagram card

--- js/app.js
+++ js/app.js
@@ -8,9 +8,27 @@ let userImg = $("<img>").attr("class", "card__user-img").attr("src", userImgSrc)
 let cardInfo = $("<div>").attr("class", "card__info");
 let cardName = $("<span>").attr("class", "card__name").text("Kesha King");
 let cardTitle = $("<span>").attr("class", "card__title").text(userJobTitle);
+let cardSocials = $("<div>").attr("class", "card__socials");
+let faceBookCard = $("<div>").attr("class", "card__icon card__icon--fb");
+let cardIconBoxFb = $("<span>").attr("class", "card__icon-box");
+let faceBookIcon = $("<i>").attr("class", "fab fa-facebook-f");
+let cardIconTitleFb = $("<span>").attr("class", "card__icon-title").text("Facebook");
+let instagramCard = $("<div>").attr("class", "card__icon card__icon--ig");
+let cardIconBoxInstagram = $("<span>").attr("class", "card__icon-box");
+let instagramIcon = $("<i>").attr("class", "fab fa-instagram");
+let cardIconTitleInstagram = $("<span>").attr("class", "card__icon-title").text("Instagram")
+
+
+cardIconBoxInstagram.append(instagramIcon);
+instagramCard.append(cardIconBoxInstagram, cardIconTitleInstagram);
+
+
+cardIconBoxFb.append(faceBookIcon);
+faceBookCard.append(cardIconBoxFb,cardIconTitleFb);
+cardSocials.append(faceBookCard,instagramCard);
 
 cardInfo.append(cardName).append(cardTitle);
-cardWrapper.append(userImg).append(cardInfo);
+cardWrapper.append(userImg).append(cardInfo).append(cardSocials);
 section.append(cardWrapper);
 contentArea.append(section);
 root.append(contentArea);
